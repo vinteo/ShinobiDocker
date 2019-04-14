6c7fff88aa16c724f3d8896bc91423a069070402
MiGoller <michael@gollerdo.de>
2019-04-14 20:24:19 +0200

    Added master release CI.

--- fake-releaseImage.sh
+++ fake-releaseImage.sh
@@ -9,12 +9,25 @@ export FAKE_CI=true
 # ./releaseImage.sh [branch1, branch2, ...] [image1, image2, ...] [DEST_IMAGE_NAME] \
 #     [APP_FLAVOR] [FORCE_APP_VERSION] [APP_SPECIAL_VERSIONS] [APP_ISDEFAULT] [APP_SPECIAL_TAGS] [APP_SUFFIX] [APP_DISTRO]
 
-# Official images: alpine and debian
+echo "----  DEV-images  ----"
+# alpine and debian images
 /bin/bash ./gitlab-ci/releaseImage.sh \
     "dev" "alpine,debian" "testimagename" \
     "" "-" "" "false" ""  ""
 
 # Offical image with tags "latest" and "official"
+/bin/bash ./gitlab-ci/releaseImage.sh \
+    "dev" "official" "testimagename" \
+    "" "-" "" "false" "latest"  ""
+
+echo "----  MASTER-images  ----"
+# alpine and debian images
+/bin/bash ./gitlab-ci/releaseImage.sh \
+    "dev" "alpine,debian" "testimagename" \
+    "" "image" "" "false" ""  ""
+
+# Offical image with tags "latest" and "official"
+#   The official image does not contain any version information!
 /bin/bash ./gitlab-ci/releaseImage.sh \
     "dev" "official" "testimagename" \
     "" "-" "" "false" "latest"  ""
